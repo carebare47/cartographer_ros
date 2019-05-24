ef240dd8b936277906a3b406a7bd5b26b77b3a27
Tom Queen <tom_q@hotmail.co.uk>
2019-05-24 10:33:10 +0100

    Update my_robot.lua

--- cartographer_ros/configuration_files/my_robot.lua
+++ cartographer_ros/configuration_files/my_robot.lua
@@ -45,5 +45,6 @@ options = {
 MAP_BUILDER.use_trajectory_builder_2d = true
 TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 10
 TRAJECTORY_BUILDER_2D.use_imu_data = false
+TRAJECTORY_BUILDER_2D.min_range = 0.65
 
 return options
